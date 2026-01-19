009704c52be53927b32b4e8feb0c74b614f291fa
Julien Schueller <schueller@phimeca.com>
2026-01-19 08:40:47 +0100

    CI: -Wno-shadow

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -6,7 +6,7 @@ cd /tmp
 cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
       -DCMAKE_UNITY_BUILD=ON \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wsuggest-override -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
-      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter" \
+      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-shadow" \
       -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
       -B build /io
 cd build
